6300ec9db769052ee5243db1789aa5e604da2d9e
dang2001 <ngohuudang2001@gmail.com>
2022-03-31 17:04:38 +0700

    delete file vscode

--- project_SE/settings.py
+++ project_SE/settings.py
@@ -75,10 +75,24 @@ WSGI_APPLICATION = 'project_SE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sample',                      
+        'USER': 'root',
+        'PASSWORD': 'Huu1234dang!@#',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'TEST' : {    
+            'NAME': 'test_sample', 
+        }
     }
 }
 
