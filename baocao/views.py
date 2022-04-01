640e15d388ffd94c50928c0fc8ec633524d26169
lcminhbu <lminhbuu@gmail.com>
2022-04-01 22:33:23 +0700

    add search report views

--- baocao/views.py
+++ baocao/views.py
@@ -6,13 +6,13 @@ def index(request):
     return render(request, 'baocao/index.html')
 
 def baoCaoMon(request):
-    return HttpResponse("Day la trang bao cao mon!")
+    return render(request, 'baocao/baocaomon.html')
 
 def taoBaoCaoMon(request):
     return render(request, 'baocao/taobaocaomon.html')
 
 def baoCaoHocKy(request):
-    return HttpResponse("Day la trang bao cao hoc ky!")
+    return render(request, 'baocao/baocaohocky.html')
 
 def taoBaoCaoHocKy(request):
     return render(request, 'baocao/taobaocaohocky.html')
