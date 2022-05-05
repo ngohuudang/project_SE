e47b0922934480050cdf771a215f93d18e945404
ngohuudang2001 <ngohuudang2001@gmail.com>
2022-05-05 07:55:58 +0700

    merge all to studentMan

--- project_SE/settings.py
+++ project_SE/settings.py
@@ -31,16 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'dangkylophoc.apps.TestappConfig',
-    'baocao.apps.BaocaoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'transcript',
-    'dangkylophoc'
+    'studentMan',
     
 ]
 
