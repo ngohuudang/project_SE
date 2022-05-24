dc29abd325c5619081fb35d99d35cf070898283c
ngohuudang2001 <ngohuudang2001@gmail.com>
2022-05-25 00:21:35 +0700

    update models.py and admin.py

--- studentMan/admin.py
+++ studentMan/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Account)
+admin.site.register(Age)
+admin.site.register(ClassOfSchool)
+admin.site.register(Subject)
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Mark)
