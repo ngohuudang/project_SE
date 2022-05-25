2885b6ac5b627c9d5fcc149c0f0fe69c10c32dd2
ngohuudang2001 <ngohuudang2001@gmail.com>
2022-05-25 09:46:57 +0700

    remove acoount at forms.py

--- studentMan/forms.py
+++ studentMan/forms.py
@@ -1,10 +1,2 @@
 from django import forms
-
-from studentMan.models import Account
-
-
-class EmployeeForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-
-    class Meta:
-        model = Account
+from .models import *
