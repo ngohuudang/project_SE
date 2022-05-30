1de399e52536fbb40d22bbe89c8b47e0b8923436
lcminhbu <lminhbuu@gmail.com>
2022-05-30 19:11:18 +0700

    them branch code chuc nang

--- studentMan/urls.py
+++ studentMan/urls.py
@@ -6,6 +6,21 @@ from . import views
 
 urlpatterns = [
 
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(),
+         name='reset_password'),
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
+
     path("home/", views.admin_home, name='admin_home'),
     path("tiepnhanhocsinh/", views.tiepNhanHS, name='tiepNhanHS'),
     path("danhsachlop/", views.dsLop, name='dsLop'),
@@ -16,6 +31,7 @@ urlpatterns = [
     path("bangdiem/", views.bangDiem, name='bangDiem'),
     path("baocaomonhoc/", views.baoCaoMH, name='baoCaoMonHoc'),
     path("baocaohocki/", views.baoCaoHK, name='baoCaoHocKi'),
+    path("baocaohocki/<str:lop>/<int:hocKy>/<str:nienKhoa>/", views.baoCaoHocKy, name='baoCaoHK'),
     path("quanlituoi/", views.quanLiTuoi, name='quanLiTuoi'),
     path("quanlilop/", views.quanLiLop, name='quanLiLop'),
     path("quanlimon/", views.quanLiMon, name='quanLiMon'),
--- studentMan/views.py
+++ studentMan/views.py
@@ -8,38 +8,93 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.forms import *
+from .report import *
+
+from .decorators import unauthenticated_user
+
 from django.contrib.auth.models import Group
+
+
 # Create your views here.
 
+@login_required(login_url='login')
 def admin_home(request):
     return render(request, 'admin_template/home_content.html')
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('admin_home')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+    context = {}
+    return render(request, 'admin_template/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
 
 def tiepNhanHS(request):
     return render(request, 'admin_template/tiepNhanHS.html')
 
+
 def dsLop(request):
     return render(request, 'admin_template/dsLop.html')
 
+
 def lapDSLop(request):
-    return render(request,'admin_template/lapDS.html')
+    return render(request, 'admin_template/lapDS.html')
+
 
 def traCuu(request):
-    return render(request,'admin_template/traCuu.html')
+    return render(request, 'admin_template/traCuu.html')
+
 
 def bangDiem(request):
     return render(request, 'admin_template/bangDiem.html')
 
+
 def baoCaoMH(request):
-    return render(request,'admin_template/baoCaoMonHoc.html')
+    return render(request, 'admin_template/baoCaoMonHoc.html')
+
+
+@unauthenticated_user
+def baoCaoHocKy(request, lop, hocKy, nienKhoa):
+    all_classes = ClassOfSchool.objects.all()
+    id = request.user.username
+    report = Report()
+    report1 = [report.show(id, lop, hocKy, nienKhoa)]
+    all_nienKhoa = Age.objects.all()
+    context = {'reports': report1,
+               'classes': all_classes,
+               'lop': lop,
+               'hocky': hocKy,
+               'nienKhoa': nienKhoa,
+               'all_nienKhoa': all_nienKhoa}
+
+    return render(request, 'admin_template/baoCaoHocKi.html', context)
+
 
 def baoCaoHK(request):
-    return render(request,'admin_template/baoCaoHocKi.html')
+    return baoCaoHocKy(request, "---", 1, 2020)
+
 
 def quanLiTuoi(request):
     return render(request, 'admin_template/quanLiTuoi.html')
 
+
 def quanLiLop(request):
-    return render(request,'admin_template/quanLiLop.html')
+    return render(request, 'admin_template/quanLiLop.html')
+
 
 def quanLiMon(request):
-    return render(request,'admin_template/quanLiMon.html')
+    return render(request, 'admin_template/quanLiMon.html')
