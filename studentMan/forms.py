ca524f4647e0c3876363dc14948788d61ae3235f
Nguyen Minh Tu <84379561+tuak123ax@users.noreply.github.com>
2022-06-02 19:34:15 +0700

    Add files via upload

--- studentMan/forms.py
+++ studentMan/forms.py
@@ -1,5 +1,25 @@
-from django import forms
+
+from attr import field
+from django.forms import ModelForm
 from .models import *
+from django import forms
+
+class transcriptForm(forms.ModelForm):
+    class Meta:
+        model = Mark
+        # fields = ['student','subject','semester_mark']
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['semester_mark'].widget.attrs.update(
+            {'class': 'form-select'}
+        )
+
+class subjectForm(forms.ModelForm):
+    class Meta:
+        model = Subject
+        fields = '__all__'
 
 class classForm(forms.ModelForm):
     class Meta:
--- studentMan/urls.py
+++ studentMan/urls.py
@@ -19,9 +19,12 @@ urlpatterns = [
     path("baocaohocki/", views.baoCaoHK, name='baoCaoHocKi'),
     path("quanlituoi/", views.quanLiTuoi, name='quanLiTuoi'),
     path("quanlilop/", views.quanLiLop, name='quanLiLop'),
-    path("quanlilop/<str:pk_test>/", views.quanLiLopFilter, name='quanLiLopFilter'),
     path("quanlimon/", views.quanLiMon, name='quanLiMon'),
+    path("quanlimon/capnhat/<int:subject_id>", views.capNhatMon, name='capNhatMon'),
+    path("subject/delete/<int:subject_id>",views.xoaMon, name='xoaMon'),
+    path("subject/add", views.themMon, name='themMon'),
     path("quanlilop/capnhat/<int:class_id>", views.capNhatLop, name='capNhatLop'),
     path("Class/delete/<int:class_id>", views.xoaLop, name='xoaLop'),
     path("Class/add", views.themLop, name='themLop'),
+    path("quanlilop/<str:pk_test>/", views.quanLiLopFilter, name='quanLiLopFilter'),
 ]
