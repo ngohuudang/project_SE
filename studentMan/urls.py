f64a426b347ff0709e59c9e32226ab43695fc6a5
thanggnguyenn <102422555+thanggnguyenn@users.noreply.github.com>
2022-06-14 21:18:37 +0700

    thêm chức năng reset mk

--- studentMan/urls.py
+++ studentMan/urls.py
@@ -26,4 +26,17 @@ urlpatterns = [
     path("subject/add", views.themMon, name='themMon'),
     path("themtaikhoangv/",views.themTaiKhoanGV,name='themTaiKhoanGV'),
     path("taotkhocsinh/",views.taoTKHocSinh,name='taoTKHocSinh')
+    
+    path('reset_password/',
+            auth_views.PasswordResetView.as_view(template_name='admin_template/capNhatMatKhau.html'),
+            name='reset_password'),
+    path('reset_password_sent/',
+            auth_views.PasswordResetDoneView.as_view(template_name='admin_template/thongBaoResetMatKhau.html'),
+            name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+            auth_views.PasswordResetConfirmView.as_view(template_name='admin_template/reset_password_form.html'),
+            name='password_reset_confirm'),
+    path('reset_password_complete/',
+            auth_views.PasswordResetCompleteView.as_view(template_name='admin_template/thongBaoResetMatKhauThanhCong.html'),
+            name='password_reset_complete'),
 ]
